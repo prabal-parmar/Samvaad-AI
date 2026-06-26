a65896d5ea522d3bf6c41a6a7b38173959960253
Prabal Parmar <prabal9713@gmail.com>
2026-06-26 23:40:17 +0530

    Add login or logout when refresh using saved token functionality

--- SamvaadAI/src/App.tsx
+++ SamvaadAI/src/App.tsx
@@ -1,10 +1,36 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import { ThemeProvider } from './context/themeContext'
+import { useEffect } from 'react'
+import { getToken, removeToken } from './api/auth/axiosInterseptor'
+import { jwtDecode } from 'jwt-decode'
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = getToken();
+    if(!token) {
+      return;
+    }
+    const decoded = jwtDecode(token);
+
+    if(!decoded){
+      removeToken();
+      // Logout
+      navigate("/login");
+      return;
+    }
+
+    if(decoded.exp && decoded.exp < Date.now()/1000){
+      removeToken();
+      // Logout
+      navigate("/login");
+      return;
+    }
+  }, [])
 
   return (
     <ThemeProvider>
