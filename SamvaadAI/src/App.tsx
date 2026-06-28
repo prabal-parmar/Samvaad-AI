52f0f25caadcbb7589ba65b8c088acf5b85e75cd
Prabal Parmar <prabal9713@gmail.com>
2026-06-28 08:40:26 +0530

    Update App.tsx by adding theme in localStore to conserve applied theme of the app and update local url of backend

--- SamvaadAI/src/App.tsx
+++ SamvaadAI/src/App.tsx
@@ -2,18 +2,29 @@ import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
-import { ThemeProvider } from './context/themeContext'
 import { useEffect } from 'react'
 import { getToken, removeToken } from './api/auth/axiosInterseptor'
 import { jwtDecode } from 'jwt-decode'
+import GuestRoute from './props/GuesRoute'
+import ProtectedRoute from './props/ProtectedRoute'
+import Home from './pages/Home'
+import { useTheme } from './context/themeContext'
 
 function App() {
+  const { setIsDark } = useTheme();
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = getToken();
     if(!token) {
       return;
+    }
+    const theme = localStorage.getItem("theme");
+    if(theme=="dark"){
+      setIsDark(true);
+    }
+    else{
+      setIsDark(false);
     }
     const decoded = jwtDecode(token);
 
@@ -33,12 +44,28 @@ function App() {
   }, [])
 
   return (
-    <ThemeProvider>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/login" 
+        element={
+          <GuestRoute>
+            <Login />
+          </GuestRoute>
+          } />
+          
+        <Route path="/register" 
+        element={
+          <GuestRoute>
+            <Register />
+          </GuestRoute>
+        } />
       </Routes>
-    </ThemeProvider>
   )
 }
 
