0a4f4edba43388955c82c1d1f04dd9aa7a9937c3
Prabal Parmar <prabal9713@gmail.com>
2026-07-01 01:58:57 +0530

    Update main.go file to add CORS policy

--- Samvaad-backend/main.go
+++ Samvaad-backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/prabal-parmar/Samvaad-AI/configs"
 	"github.com/prabal-parmar/Samvaad-AI/internal/routes"
@@ -9,7 +12,29 @@ import (
 func main() {
 
 	configs.ConnectDB();
+
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"PATCH",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Accept",
+			"Authorization",
+		},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	routes.SetupRoutes(router);
 
