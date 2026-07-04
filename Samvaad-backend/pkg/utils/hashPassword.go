50b4b25119205c56c5cc68f48b194047f596198b
Prabal Parmar <prabal9713@gmail.com>
2026-07-04 12:44:07 +0530

    Add hash password function to hash and verify password.

---
+++ Samvaad-backend/pkg/utils/hashPassword.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return "", err;
+	}
+
+	return string(hashed), nil;
+}
+
+func ComparePassword(hashedPassword string, password string) (bool, error) {
+	if password == "" {
+		return false, errors.New("Didn't receive password!")
+	}
+
+	err:=bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
