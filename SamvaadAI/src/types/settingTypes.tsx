95c7c5aece93378900e75950a5f963026d4f8112
Prabal Parmar <prabal9713@gmail.com>
2026-07-05 10:29:06 +0530

    Add setting types currently for theme token.

---
+++ SamvaadAI/src/types/settingTypes.tsx
@@ -0,0 +1,11 @@
+export interface ThemeTokens {
+  bg: string;
+  panel: string;
+  textMain: string;
+  textMuted: string;
+  border: string;
+  accentText: string;
+  accentBg: string;
+  accentSubtle: string;
+  hoverSubtle: string;
+}
