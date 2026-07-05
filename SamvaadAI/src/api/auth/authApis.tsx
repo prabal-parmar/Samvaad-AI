7a4b51b6ec9b00f431c128e84d5f93bd9c1d91eb
Prabal Parmar <prabal9713@gmail.com>
2026-07-05 20:50:12 +0530

    Add auth api for logout

--- SamvaadAI/src/api/auth/authApis.tsx
+++ SamvaadAI/src/api/auth/authApis.tsx
@@ -1,6 +1,6 @@
 import type { LoginDataTypes } from "../../types/loginTypes";
 import type { RegisterUserDataTypes } from "../../types/registerTypes";
-import { api } from "./axiosInterseptor";
+import { api, getToken, removeToken } from "./axiosInterseptor";
 
 export const registerUser = async (userData: RegisterUserDataTypes) => {
     try {
@@ -18,15 +18,27 @@ export const loginUser = async (userData: LoginDataTypes) => {
         const isEmail = emailRegex.test(userData.userInp.trim());
 
         if(isEmail){
-            const response = await api.post('/auth/login', {email: userData.userInp, password: userData.password});
+            const response = await api.post('/auth/login', {email: userData.userInp.toLowerCase(), password: userData.password});
             return [response.data.token, response.data.username, "Logged In Successfully"]
         }
         else {
-            const response = await api.post('/auth/login', {username: userData.userInp, password: userData.password});
+            const response = await api.post('/auth/login', {username: userData.userInp.toLowerCase(), password: userData.password});
             return [response.data.token, response.data.username, "Logged In Successfully"]
         }
     } catch (error) {
         console.log(error);
         return [null, "", "Something went wrong!"];
+    }
+}
+
+export const logout = async () => {
+    try {
+        if(getToken()){
+            removeToken();
+        }
+        return true;
+    } catch (error) {
+        console.log(error)
+        return false;
     }
 }
